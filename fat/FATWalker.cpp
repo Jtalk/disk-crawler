32db386b8fe783fdf2e19549840ae5b402daae17
Jtalk <me@jtalk.me>
2013-11-03 13:52:13 +0600

    ???????? ?????? ? FAT.

--- fat/FATWalker.cpp
+++ fat/FATWalker.cpp
@@ -19,10 +19,13 @@
 
 #include "FATWalker.h"
 
+#include "utility.h"
+
 using namespace std;
 
 template<typename Target>
-Target to(const FATWalker::bytes_t &bytes) {
+Target to(const FATWalker::bytes_t &bytes) 
+{
         Target value = *reinterpret_cast<const Target*>(&bytes[0]);
         return value;
 }
@@ -34,7 +37,8 @@ FATWalker::FATWalker(const string& device_name):
 FATWalker::~FATWalker()
 {}
 
-FATWalker::bytes_t FATWalker::read(FATWalker::Offset offset, size_t bytes_to_read) const {
+FATWalker::bytes_t FATWalker::read(FATWalker::Offset offset, size_t bytes_to_read) const 
+{
         bytes_t buffer(bytes_to_read, 0);
         
         this->device.seekg(static_cast<size_t>(offset));
@@ -43,21 +47,59 @@ FATWalker::bytes_t FATWalker::read(FATWalker::Offset offset, size_t bytes_to_rea
         return buffer;
 }
 
-uint16_t FATWalker::sector_size() const {
+uint16_t FATWalker::sector_size() const 
+{
         auto bytes = this->read(SECTOR_SIZE, 2);
         return to<uint16_t>(bytes);
 }
 
-uint8_t FATWalker::sectors_per_cluster() const {
+uint8_t FATWalker::sectors_per_cluster() const 
+{
         auto bytes = this->read(SECTORS_PER_CLUSTER, 1);
         return to<uint8_t>(bytes);;
 }
 
-size_t FATWalker::cluster_size() const {
+size_t FATWalker::cluster_size() const 
+{
         return this->sector_size() * this->sectors_per_cluster();
 }
 
-uint8_t FATWalker::tables_count() const {
+uint8_t FATWalker::tables_count() const 
+{
         auto bytes = this->read(NUMBER_OF_TABLES, 1);
-        return to<uint8_t>(bytes);;        
+        return to<uint8_t>(bytes);    
+}
+
+uint16_t FATWalker::root_entries_count() const 
+{
+        auto bytes = this->read(NUMBER_OF_ROOT_DIRECTORY_ENTRIES, 2);
+        return to<uint16_t>(bytes);        
+}
+
+uint32_t FATWalker::file_size(const std::vector< uint8_t >& dir_entry)
+{
+        constexpr size_t ENTRY_SIZE = 32;
+        
+        utility::assert(dir_entry.size() < ENTRY_SIZE, "Invalid directory entry size");
+        
+        return to<uint32_t>(&dir_entry[FILE_SIZE]);
+}
+
+void FATWalker::find(const byte_array_t& to_find, std::list<result_t> &found)
+{
+        auto found = this->find_by_signatures();
+        
+        for (auto &file : found) {
+                const bytes_t &file_data = this->traceback(file);
+                size_t pos = utility::find(byte_stream_t(file_data), to_find);
+                if (pos != bytes_t::npos) {
+                        found.push_back({file_data, pos});
+                }
+        }
+}
+
+
+bool FATWalker::operator!() const
+{
+        return !this->device;
 }
--- fat/FATWalker.h
+++ fat/FATWalker.h
@@ -24,12 +24,20 @@
 class FATWalker {       
 public:
         typedef std::basic_string<uint8_t> bytes_t;
+        typedef std::basic_stringstream<uint8_t> byte_stream_t;
+        typedef std::pair<bytes_t, size_t> result_t;
        
 private:
         enum Offset {
                 SECTOR_SIZE = 0x0B,
                 SECTORS_PER_CLUSTER = 0x0D, 
-                NUMBER_OF_TABLES = 0x010
+                NUMBER_OF_TABLES = 0x010, 
+                NUMBER_OF_ROOT_DIRECTORY_ENTRIES = 0x011
+        };
+        
+        enum DirectoryEntryOffset {
+                FILE_NAME = 0x0, 
+                FILE_SIZE = 0x1c
         };
                 
         mutable std::ifstream device;
@@ -50,4 +58,12 @@ public:
         size_t cluster_size() const;
         
         uint8_t tables_count() const;
+        
+        uint16_t root_entries_count() const;
+        
+        uint32_t file_size(const std::vector<uint8_t> &dir_entry);
+        
+        size_t find(const byte_array_t &to_find);
+        
+        bool operator ! () const;
 };
