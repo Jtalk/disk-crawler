b14a2ce3944849b9fea0d2338c26a31b7eaeed51
Jtalk <Jtalk@localhost>
2014-05-16 21:38:19 +0600

    ????????? ??????? ??????-?????.

--- base/utility_rabin_karp.cpp
+++ base/utility_rabin_karp.cpp
@@ -43,7 +43,7 @@ static uint32_t rk_pow_mod(uint32_t base, uint32_t exp, uint32_t modulus) {
 	return result;
 }
 
-static rk_powers(powers_cache_t &cache, size_t chunk_size) {
+static void rk_powers(powers_cache_t &cache, size_t chunk_size) {
 	cache.clear();
 	cache.reserve(chunk_size);
 	for (size_t i = 0; i < chunk_size; i++) {
@@ -56,36 +56,26 @@ static bool rk_chunk_size_compare(const search_terms_t::value_type &fst, const s
 }
 
 struct rk_hash {
-	typedef std::pair<byte_array_t::const_iterator, byte_array_t::const_iterator> iterators_t;
 	powers_cache_t cache;
-	bool reuse;
-	bool new_round;
-	size_t old_hash;
 	
-	size_t operator ()(const iterators_t &iterators) const {
-		if (this->reuse) {
-			return this->reuse_hash(iterators);
-		}
-		if (not this->new_round) {
-			return this->old_hash;
-		}
+	size_t new_hash(Buffer::const_iterator first) const {
 		uint32_t hash = 0;
 		for (int32_t i = this->cache.size() - 1; i >= 0; i++) {
-			hash = (hash + *(iterators.first++) * this->cache[i]) % RK_MODULUS;
+			hash = (hash + *(first++) * this->cache[i]) % RK_MODULUS;
 		}
-		this->old_hash = hash;
-		this->new_round = false;
 		return hash;
 	}
 	
-	size_t reuse_hash(const iterators_t &iterators) const {
-		auto removing = *(iterators.first - 1);
+	size_t reuse_hash(size_t old_hash, uint8_t removing, uint8_t adding, size_t chunk_size) {
+		uint32_t removing_value = removing * this->cache[chunk_size - 1] % RK_MODULUS;
+		if (removing_value > old_hash) {
+			old_hash = old_hash += RK_MODULUS;
+		}
+		old_hash -= removing_value;
+		uint32_t adding_value = adding * this->cache[0] % RK_MODULUS;
+		old_hash = (old_hash + adding_value) % RK_MODULUS;
+		return old_hash;
 	}
-	
-	rk_hash(const rk_hash&) = delete;
-	rk_hash(rk_hash&&) = delete;
-	rk_hash &operator=(const rk_hash&) = delete;
-	rk_hash &operator=(rk_hash&&) = delete;
 };
 	
 namespace utility {
@@ -93,34 +83,51 @@ namespace utility {
 SearchResult rabin_karp(const Buffer &string, const search_terms_t &to_find) {
 	size_t chunk_size = std::min_element(CONTAINER(to_find), rk_chunk_size_compare)->size();
 	
-	rk_hash hasher;
-	hasher.reuse = false;
-	hasher.new_round = true;
-	rk_powers(hasher.cache, chunk_size);
-	
-	std::unordered_map<rk_hash::iterators_t, size_t, rk_hash> patterns(to_find.size() * RK_HASH_TABLE_FACTOR, hasher);
-	size_t i = 0;
-	for (auto &pattern : to_find) {
-		patterns.insert({{pattern.cbegin(), pattern.cend()}, i++});
+	if (string.size() < chunk_size) {
+		return {-1, Buffer::npos};
 	}
 	
-	size_t current_pos = 0;
-	while (true) {
-		rk_hash::iterators_t iters{string.cbegin() + current_pos, string.cbegin() + current_pos + chunk_size};
-		if (iters.second >= string.cend()) {
-			break;
+	rk_hash hasher;
+	rk_powers(hasher.cache, chunk_size);
+	
+	typedef std::vector<size_t> collisions_t;
+	typedef std::pair<Buffer::const_iterator, Buffer::const_iterator> iterators_t;
+	
+	std::unordered_map<size_t, collisions_t> patterns(to_find.size() * RK_HASH_TABLE_FACTOR);
+	size_t i = 0;
+	for (auto &pattern : to_find) {
+		uint32_t hash = hasher.new_hash(pattern.data());
+		patterns[hash].push_back(i++);
+	}
+	
+	uint32_t hash = 0;
+	for (size_t current_pos = 0; string.cbegin() + current_pos < string.cend() - chunk_size + 1; current_pos++) {
+		iterators_t iters{string.cbegin() + current_pos, string.cbegin() + current_pos + chunk_size};
+		if (current_pos == 0) {
+			hash = hasher.new_hash(string.cbegin());
+		} else {
+			hash = hasher.reuse_hash(hash, *(iters.first - 1), *(iters.second - 1), chunk_size);
 		}
-		if (hasher.reuse) {
-			hasher.removing = *(iters.first - 1);
-			hasher.adding = *(iters.second - 1);
-			hasher.new_round = true;
-		}
-		auto found = patterns.find(iters);
+		auto found = patterns.find(hash);
 		if (found != patterns.cend()) {
-			return {found->second, current_pos};
+			for (const auto &index : found->second) {
+				const auto &pattern = to_find[index];
+				size_t buffer_left = string.cend() - string.cbegin() - current_pos;
+				if (buffer_left < pattern.size()) {
+					continue;
+				}
+				bool matched = true;
+				for (size_t i = 0; i < pattern.size(); i++) {
+					if (pattern[i] != iters.first[i]) {
+						matched = false;
+						break;
+					} 
+				}
+				if (matched) {
+					return {int64_t(index), current_pos};
+				}
+			}
 		}
-		current_pos++;
-		hasher.reuse = true;
 	}
 	return {-1, Buffer::npos};
 }
